8c323953245f94d37462f054ce766625ed72ae0c
denya54 <denis-khvesenya@yandex.com>
2021-12-07 14:42:15 +0300

    make page404

--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import s from './Error404.module.css'
+import picture from './kr0o1h5.jpg'
 
 function Error404() {
     return (
-        <div>
+        <div className={s.error}>
+            Attention
             <div>404</div>
             <div>Page not found!</div>
             <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
+            <img src={picture}/>
         </div>
     )
 }
