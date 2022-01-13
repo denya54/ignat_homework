56b2e920f9455bb4cc7e177051b0ff6b0527d6a0
denya54 <denis-khvesenya@yandex.com>
2022-01-13 23:03:37 +0300

    complete 8 homework

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,52 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+
+const sortUpper = (a: UserType, b: UserType) => {
+    if (a.name > b.name) return 1
+    else if (a.name < b.name) return -1
+    else return 0
+}
+
+const sortDowner = (a: UserType, b: UserType) => {
+    if (a.name > b.name) return -1
+    else if (a.name < b.name) return 1
+    else return 0
+}
+
+export const homeWorkReducer = (state: Array<UserType>, action: ActionType): Array<UserType> => {
     switch (action.type) {
         case 'sort': {
-            // need to fix
+            let copyState = [...state]
+            if (action.payload === 'up') {
+              return copyState.sort(sortUpper)
+            } if (action.payload === 'down') {
+                return copyState.sort(sortDowner)
+            }
             return state
         }
         case 'check': {
-            // need to fix
-            return state
+            let copyState = [...state]
+            return copyState.filter(us => us.age > action.payload)
         }
         default: return state
     }
 }
+
+export const checkAC = (payload: number) => {
+    return {
+        type: 'check' as const,
+        payload
+    }
+}
+
+export const sortAC = (payload: 'up' | 'down') => {
+    return {
+        type: 'sort' as const,
+        payload
+    }
+}
+
+type checkACReturnType = ReturnType<typeof checkAC>
+type sortACReturnType = ReturnType<typeof sortAC>
+
+type ActionType = checkACReturnType | sortACReturnType
--- src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
+++ src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import {homeWorkReducer} from '../homeWorkReducer'
+import {checkAC, homeWorkReducer, sortAC} from '../homeWorkReducer'
+import {UserType} from "../../HW8";
 
-let initialState: any[] // need to fix any
+let initialState: Array<UserType>  // need to fix any
 
 beforeEach(() => {
     initialState = [
@@ -15,18 +16,29 @@ beforeEach(() => {
 })
 
 test('sort name up', () => {
-    const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'up'})
-
+    const newState: Array<UserType> = homeWorkReducer(initialState, sortAC('up'))
     console.log(newState)
-    // expect(...).toBe(...)
+    expect(newState).toStrictEqual([
+        {_id: 1, name: 'Александр', age: 66},
+        {_id: 3, name: 'Виктор', age: 44},
+        {_id: 4, name: 'Дмитрий', age: 40},
+        {_id: 5, name: 'Ирина', age: 55},
+        {_id: 2, name: 'Коля', age: 16},
+        {_id: 0, name: 'Кот', age: 3},
+    ])
 })
 test('sort name down', () => {
-    const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'down'})
-
-
+    const newState:  Array<UserType>  = homeWorkReducer(initialState, {type: 'sort', payload: 'down'})
+    expect(newState).toStrictEqual([
+        {_id: 0, name: 'Кот', age: 3},
+        {_id: 2, name: 'Коля', age: 16},
+        {_id: 5, name: 'Ирина', age: 55},
+        {_id: 4, name: 'Дмитрий', age: 40},
+        {_id: 3, name: 'Виктор', age: 44},
+        {_id: 1, name: 'Александр', age: 66},
+    ])
 })
 test('check age 18', () => {
-    const newState = homeWorkReducer(initialState, {type: 'check', payload: 18})
-
-
+    const newState: Array<UserType> = homeWorkReducer(initialState, checkAC(18))
+    expect(newState.length).toBe(4)
 })
