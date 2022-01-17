e28d6b759abb034cbfad2d373e007c0418020d4f
denya54 <denis-khvesenya@yandex.com>
2022-01-17 17:07:29 +0300

    complete 9 homework

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -3,29 +3,26 @@ import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date())
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearInterval(timerId)
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date())
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
-
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
 
     return (
         <div>
@@ -33,12 +30,13 @@ function Clock() {
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
             >
-                {stringTime}
+                {/*{date.getHours()}:{date.getMinutes()}:{date.getSeconds()}*/}
+                {date.toLocaleTimeString()}
             </div>
 
             {show && (
                 <div>
-                    {stringDate}
+                    {date.toLocaleDateString()}
                 </div>
             )}
 
